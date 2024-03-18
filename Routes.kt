265d249b92cadf4be74def26e7ccf5fb158c45ac
Bernard Joseph Roche <84322289+bjrochem72@users.noreply.github.com>
2024-03-18 17:57:30 +0000

    Add files via upload

---
+++ Routes.kt
@@ -0,0 +1,7 @@
+package com.mod72.simplenavigation
+
+sealed class Routes(val route: String) {
+    object FirstScreen : Routes("first_screen")
+    object SecondScreen : Routes("second_screen")
+    object ThirdScreen : Routes("third_screen")
+}
